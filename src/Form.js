005b444e6f757c21d17bc5d560005486835b4de1
dr-dolce14 <dolcemascop1@gmail.com>
2021-02-26 11:47:19 -0500

    displaying results

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,55 @@
 import React, {useState, useEffect} from 'react'
+import Result from './Result'
 
 const Form = () => {
     const [term, setTerm] = useState(0)
     const [results, setResults] = useState([])
 
-  const finalResults = (e) => {
-      e.preventDefault()
-      console.log(parseInt(term) + 2)
-  }
+//   const finalResults = (e) => {
+//       e.preventDefault()
+//       for (let i = 1; i <= term; i++) {
+//         //is the number a multiple of 3 and 5?
+//         if (i % 3 === 0 && i % 5 === 0) {  //could also just do i % 15 === 0
+//             console.log('fizzbuzz');
+//             //  if we meet this case, we don't any execute any other code inside the for loop...so we use else if statements instead of separate if statements
+//         } else if (i  % 3 === 0) {
+//             console.log('fizz');
+//         } else if (i % 5 === 0) {
+//             console.log('buzz');
+//         } else {
+//             console.log(i);
+//         }
+//     }
+//   }
+
+function fizzBuzz(n) {
+    let arr = []
+    // can't do (let x of y) because there isn't anything to iterate over
+    for (let i = 1; i <= n; i++) {
+        //is the number a multiple of 3 and 5?
+        if (i % 3 === 0 && i % 5 === 0) {  //could also just do i % 15 === 0
+            arr.push('fizzbuzz');
+            //  if we meet this case, we don't any execute any other code inside the for loop...so we use else if statements instead of separate if statements
+        } else if (i  % 3 === 0) {
+            arr.push('fizz');
+        } else if (i % 5 === 0) {
+            arr.push('buzz');
+        } else {
+            arr.push(i);
+        }
+    }
+    setResults(arr)
+}
+    const finalResults = (e) => {
+        e.preventDefault()
+        return fizzBuzz(term)
+    }
+
+   
+
+  
+
+
     return (
         <div>
             <div className="ui form">
@@ -23,6 +65,8 @@ const Form = () => {
             </div>
         </div>
         <div className="ui celled list">
+            <Result results={results}/>
+            
         </div>
         </div>
         )
---
+++ src/Result.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class Result extends React.Component {
+
+   renderedResults = () => {
+       return this.props.results.map((result, index) => {
+           return (
+           <div key={index} className='item'>
+               <div className='content'>
+                   <div className='header'>
+                       {result}
+                   </div>
+               </div>
+
+           </div>
+           )
+       })
+   }
+
+    render() {
+
+        return (
+            <h1>{this.renderedResults()}</h1>
+        )
+    }
+}
+
+export default Result;
